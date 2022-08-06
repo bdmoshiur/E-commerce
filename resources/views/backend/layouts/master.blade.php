b37391635ca1317dfada036d79d0a218d4d416ed
Moshiur Rahman <moshiurcse888@gmail.com>
2022-08-06 22:09:47 +0600

    update

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel Project Dashboard</title>
+  <title>Ecommerce Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="{{ asset('backend') }}/https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
